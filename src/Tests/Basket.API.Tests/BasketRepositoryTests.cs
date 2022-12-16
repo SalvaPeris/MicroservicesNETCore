0e9ea34d13cef5e2bbcd86c46f9d50f804c556ab
Salva Peris Gimeno <sperisgimeno@gmail.com>
2022-12-16 18:59:14 +0100

    Basket.API.Tests added.

---
+++ src/Tests/Basket.API.Tests/BasketRepositoryTests.cs
@@ -0,0 +1,93 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repository;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basket.API.Tests
+{
+    [TestClass]
+    public class ProductRepositoryTests
+    {
+        private IDistributedCache? _redisCache;
+        private IBasketRepository? _repository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+            var _configuration = TestConfiguration.GetConfiguration();
+            services.AddStackExchangeRedisCache(options =>
+             {
+                 options.Configuration = _configuration.GetValue<string>("CacheSettings:ConnectionString");
+             });
+
+            var provider = services.BuildServiceProvider();
+
+            _redisCache = provider.GetService<IDistributedCache>();
+            _repository = new BasketRepository(_redisCache!);
+        }
+
+        [TestMethod]
+        public async Task GetBasketAsync_Success()
+        {
+            //Act 
+            var basket = await _repository!.GetBasket("testuser");
+
+            //Assert 
+            Assert.IsNull(basket);
+        }
+
+        [TestMethod]
+        public async Task UpdateBasketAsync_Success()
+        {
+            //Act 
+            ShoppingCart newBasket = new()
+            {
+                UserName = "testuser",
+                Items =
+                {
+                    new ShoppingCartItem()
+                    {
+                        Quantity = 2,
+                        Price = 34,
+                        Color = "Red",
+                        Description = "Description mobile",
+                        ProductId = "602d2149e773f2a3990b47f5",
+                        ProductName = "iPhone X"
+                    },
+                    new ShoppingCartItem()
+                    {
+                        Quantity = 1,
+                        Price = 340,
+                        Color = "Red",
+                        Description = "Description mobile 2",
+                        ProductId = "602d2149e773f2a3990b47f6",
+                        ProductName = "iPhone XV"
+                    }
+                }
+            };
+
+            var basket = await _repository!.UpdateBasket(newBasket);
+
+            //Assert 
+            Assert.AreEqual(2, basket.Items.Count);
+            Assert.AreEqual("testuser", basket.UserName);
+            Assert.AreEqual(408, basket.TotalPrice);
+        }
+
+        [TestMethod]
+        public async Task DeleteBasketAsync_Success()
+        {
+            //Act 
+            await _repository!.DeleteBasket("testuser");
+
+            var basket = await _repository!.GetBasket("testuser");
+
+            //Assert 
+            Assert.IsNull(basket);
+        }
+
+    }
+}
---
+++ src/Tests/Basket.API.Tests/TestConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Basket.API.Tests
+{
+    public static class TestConfiguration
+    {
+        public static IConfiguration GetConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+              .SetBasePath("C:\\Users\\speri\\source\\repos\\MicroservicesNETCore\\src\\Services\\Basket\\Basket.API")
+              .AddJsonFile("appsettings.json")
+              .Build();
+            return config;
+        }
+    }
+}
