380dccbeca063f7353049b8a55ac4bdb0b539469
Salvador Peris Gimeno <sperisgimeno@gmail.com>
2022-12-30 19:27:08 +0100

    DiscountController finished.

--- src/Services/Discount/Discount.API/Controllers/DiscountController.cs
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -13,7 +13,7 @@ namespace Discount.API.Controllers
 
         public DiscountController(IDiscountRepository repository)
         {
-            _repository = repository;
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
         }
 
         [HttpGet("{productName}", Name = "GetDiscount")]
@@ -37,6 +37,13 @@ namespace Discount.API.Controllers
         public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
         {
             return Ok(await _repository.UpdateDiscount(coupon));
+        }
+
+        [HttpDelete("{productName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            return Ok(await _repository.DeleteDiscount(productName));
         }
     }
 }
