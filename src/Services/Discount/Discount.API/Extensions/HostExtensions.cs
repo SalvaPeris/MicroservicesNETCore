b8478a60e72905ff296c43252e8d8d22bb138df1
Salvador Peris Gimeno <sperisgimeno@gmail.com>
2023-01-01 21:55:22 +0100

    Migration Postgresql method  added.

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Npgsql;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static void MigrateDatabase(this IServiceProvider serviceProvider, int? retry = 0)
+        {
+            int retryForAvailibility = retry!.Value;
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                logger.LogInformation("Migration Postgresql started.");
+
+                using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                connection.Open();
+
+                using var command = new NpgsqlCommand
+                {
+                    Connection = connection
+                };
+
+                command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                command.ExecuteNonQuery();
+
+                command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                            ProductName VARCHAR(24) NOT NULL,
+                                                            Description TEXT,
+                                                            Amount INT)";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                command.ExecuteNonQuery();
+
+                logger.LogInformation("Migration Postgresql finished.");
+            }
+            catch (NpgsqlException exception)
+            {
+                logger.LogError(exception, " An error occurred while migrating Postgresql database.");
+
+                if(retryForAvailibility < 10)
+                {
+                    retryForAvailibility++;
+                    Thread.Sleep(3000);
+                    serviceProvider.MigrateDatabase();
+                }
+            }
+        }
+    }
+}
--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,4 +1,5 @@
 using Discount.API.Repositories;
+using Discount.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<ILogger>(s => s.GetService<ILogger<Program>>());
 
 var app = builder.Build();
 
@@ -20,5 +22,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.Services.MigrateDatabase();
 
 app.Run();
