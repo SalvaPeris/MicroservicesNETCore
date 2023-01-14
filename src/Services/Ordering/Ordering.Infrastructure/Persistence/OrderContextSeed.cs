33f994f99d75041a013c7be43a56f480cae56074
Salvador Peris Gimeno <sperisgimeno@gmail.com>
2023-01-14 18:45:07 +0100

    In Ordering.Infrastructure, OrderContext and Seed added.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database success with {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order()
+                {
+                    UserName = "spg",
+                    FirstName = "Salva",
+                    LastName = "Peris",
+                    EmailAddress = "sperisgimeno@gmail.com",
+                    AddressLine = "Albal",
+                    Country = "Spain"
+                }
+            };
+        }
+    }
+}
