c11ca8b691932706a56d600f7e34acf5c41ae8af
Salvador Peris Gimeno <sperisgimeno@gmail.com>
2023-01-16 09:24:01 +0100

    In Ordering.API, MigrateDatabase generic finished.

---
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ordering.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static void MigrateDatabase<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder, int? retry = 0) where TContext: DbContext
+        {
+            int retryForAvailability = retry!.Value;
+
+            using var scope = host.Services.CreateScope();
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+            try
+            {
+                logger.LogInformation("Migration {DbContextName} started", typeof(TContext));
+                InvokeSeeder(seeder!, context!, services);
+                logger.LogInformation("Migration {DbContextName} finished", typeof(TContext));
+            }
+            catch (SqlException e)
+            {
+                logger.LogInformation("An error ocurred with database migration: {e}", e);
+
+                if (retryForAvailability < 50)
+                {
+                    retryForAvailability++;
+                    Thread.Sleep(2000);
+                    MigrateDatabase<TContext>(host, seeder, retryForAvailability);
+                }
+            }
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
-
+using Ordering.API.Extensions;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +24,11 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MigrateDatabase<OrderContext>((context, service) =>
+{
+    var logger = app.Services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger!).Wait();
+});
 
 app.Run();
