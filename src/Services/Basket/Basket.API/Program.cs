4739f17cdd3ca11ecec0794ff42758b98002d4ae
Salvador Peris Gimeno <sperisgimeno@gmail.com>
2023-01-17 09:40:22 +0100

    In Basket.API, Program.cs and appsettings.json changed for RabbitMQ.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repository;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,19 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
         options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"])
     );
 builder.Services.AddScoped<IDiscountGrpcService, DiscountGrpcService>();
+
+// MassTransit / RabbitMQ Configuration
+builder.Services.AddMassTransit(configuration =>
+{
+    configuration.UsingRabbitMq((context, configurator) =>
+    {
+        configurator.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+    });
+});
+
+//New version no longer requieres the AddMassTransitHostedService -> https://stackoverflow.com/questions/72403579/workerservice-configure-a-rabbitmq-with-masstransit
+//builder.Services.AddMassTransitHostedService();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
