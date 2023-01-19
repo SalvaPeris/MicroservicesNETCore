bbf1b2dc1baa04ccfef2c48f7281281eb770b48b
Salvador Peris Gimeno <sperisgimeno@gmail.com>
2023-01-19 15:55:26 +0100

    OcelotAPIGateway project modified.

--- src/APIGateways/OcelotAPIGateways/OcelotAPIGateway/Program.cs
+++ src/APIGateways/OcelotAPIGateways/OcelotAPIGateway/Program.cs
@@ -1,6 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-app.Run();
+internal class Program
+{
+	async static Task Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+			{
+				loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+				loggingBuilder.AddConsole();
+				loggingBuilder.AddDebug();
+			})
+			.ConfigureServices(services =>
+			{
+				services.AddOcelot();
+			});
+
+		var app = builder.Build();
+
+		await app.UseOcelot();
+
+		app.Run();
+	}
+}
